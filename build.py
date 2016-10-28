584f46f691a0bfb356aa4d4eafd1e9c18563d7f1
balashovartem <balashov.artem@gmail.com>
2016-10-28 15:56:53 +0700

    Update build.py

--- build.py
+++ build.py
@@ -4,9 +4,4 @@ if __name__ == "__main__":
 
     builder = ConanMultiPackager()
     builder.add_common_builds( pure_c=True )
-    filtered_builds = []
-    for settings, options in builder.builds:
-        if settings["compiler.version"] == "4.8" or settings["compiler.version"] == "5.2":
-             filtered_builds.append([settings, options])
-    builder.builds = filtered_builds
     builder.run()
